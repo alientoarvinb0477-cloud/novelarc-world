3d641517bac1c0bb8796888a3b63bbd73283aba7
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-22 18:56:09 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // This tells Next.js 16 to allow Webpack even though Turbopack is default
   webpack: (config) => {
     config.experiments = {
       ...config.experiments,
@@ -10,11 +9,6 @@ const nextConfig: NextConfig = {
     };
     return config;
   },
-
-  // This silences the "Turbopack vs Webpack" error
-  turbo: {},
-
-  // Keeps the build moving past minor type warnings
   typescript: {
     ignoreBuildErrors: true,
   },
