310c2ca85f6e9b9ab484fdb435d197e665cdee2a
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-22 18:16:03 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   webpack: (config) => {
     config.experiments = {
@@ -11,8 +9,6 @@ const nextConfig: NextConfig = {
     };
     return config;
   },
-  // This explicitly tells Next.js we are okay with using Webpack
-  // and silences the Turbopack requirement.
   experimental: {} 
 };
 
