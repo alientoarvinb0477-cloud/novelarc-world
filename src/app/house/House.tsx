0c0b0912ee248d7071dabb4d13a12d83566c783b
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-23 16:45:56 +0800

    Update House.tsx

--- src/app/house/House.tsx
+++ src/app/house/House.tsx
@@ -1,20 +1,22 @@
 "use client";
 
 import React, { useState } from "react";
-import { useGLTF, Text } from "@react-three/drei";
+import { Text } from "@react-three/drei";
 import { RigidBody, CuboidCollider } from "@react-three/rapier";
 import { useStore } from "../../hooks/useStore";
 
-export default function House({ id, modelPath, position, doorOffset = [0, 0, 2] }: any) {
-  const { scene } = useGLTF(modelPath);
+export default function House({ id, position, doorOffset = [0, 0, 2] }: any) {
   const setNearDoor = useStore((state) => state.setNearDoor);
   const [showHint, setShowHint] = useState(false);
 
   return (
     <group position={position}>
-      {/* THE HOUSE MODEL */}
-      <RigidBody type="fixed" colliders="trimesh">
-        <primitive object={scene.clone()} />
+      {/* PLACEHOLDER BOX (Until you get a .glb) */}
+      <RigidBody type="fixed" colliders="cuboid">
+        <mesh castShadow receiveShadow>
+          <boxGeometry args={[4, 4, 4]} />
+          <meshStandardMaterial color="brown" />
+        </mesh>
       </RigidBody>
 
       {/* THE DOOR SENSOR */}
@@ -23,18 +25,18 @@ export default function House({ id, modelPath, position, doorOffset = [0, 0, 2] 
         colliders={false}
         position={doorOffset}
         onIntersectionEnter={() => {
-          setNearDoor(id); // Tell the store we are at THIS house
+          setNearDoor(id);
           setShowHint(true);
         }}
         onIntersectionExit={() => {
-          setNearDoor(null); // Clear the store when we walk away
+          setNearDoor(null);
           setShowHint(false);
         }}
       >
         <CuboidCollider args={[1.5, 2, 1.5]} sensor />
         
         {showHint && (
-          <Text position={[0, 3, 0]} fontSize={0.4} color="orange" font="/fonts/bold.ttf">
+          <Text position={[0, 3, 0]} fontSize={0.4} color="orange">
             TAP TO ENTER
           </Text>
         )}
