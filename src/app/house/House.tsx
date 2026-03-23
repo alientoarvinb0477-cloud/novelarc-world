949f4928040701ef931a25f8348d4154ee2b1096
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-23 17:38:37 +0800

    Update House.tsx

--- src/app/house/House.tsx
+++ src/app/house/House.tsx
@@ -5,25 +5,37 @@ import { Text } from "@react-three/drei";
 import { RigidBody, CuboidCollider } from "@react-three/rapier";
 import { useStore } from "../../hooks/useStore";
 
-export default function House({ id, position, doorOffset = [0, 0, 2] }: any) {
+export default function House({ id, position = [0, 0, 0] }: any) {
   const setNearDoor = useStore((state) => state.setNearDoor);
   const [showHint, setShowHint] = useState(false);
 
   return (
     <group position={position}>
-      {/* PLACEHOLDER BOX (Until you get a .glb) */}
+      {/* 1. MAIN STRUCTURE (The Walls) */}
       <RigidBody type="fixed" colliders="cuboid">
-        <mesh castShadow receiveShadow>
-          <boxGeometry args={[4, 4, 4]} />
-          <meshStandardMaterial color="brown" />
+        <mesh castShadow receiveShadow position={[0, 2, 0]}>
+          <boxGeometry args={[6, 4, 6]} />
+          <meshStandardMaterial color="#5c4033" /> {/* Dark Wood Color */}
+        </mesh>
+
+        {/* 2. THE ROOF (Flat Modern Style) */}
+        <mesh castShadow position={[0, 4.2, 0]}>
+          <boxGeometry args={[6.5, 0.5, 6.5]} />
+          <meshStandardMaterial color="#222" />
+        </mesh>
+
+        {/* 3. THE DOOR VISUAL (So you know where to walk) */}
+        <mesh position={[0, 1, 3.01]}>
+          <boxGeometry args={[1.2, 2, 0.1]} />
+          <meshStandardMaterial color="#8b4513" />
         </mesh>
       </RigidBody>
 
-      {/* THE DOOR SENSOR */}
+      {/* 4. THE DOOR SENSOR (Invisible Interaction Zone) */}
       <RigidBody
         type="fixed"
         colliders={false}
-        position={doorOffset}
+        position={[0, 1, 4]} // Placed 1 unit in front of the door
         onIntersectionEnter={() => {
           setNearDoor(id);
           setShowHint(true);
@@ -36,7 +48,12 @@ export default function House({ id, position, doorOffset = [0, 0, 2] }: any) {
         <CuboidCollider args={[1.5, 2, 1.5]} sensor />
         
         {showHint && (
-          <Text position={[0, 3, 0]} fontSize={0.4} color="orange">
+          <Text 
+            position={[0, 2.5, 0]} 
+            fontSize={0.4} 
+            color="#fb923c" // Orange-400
+            anchorX="center"
+          >
             TAP TO ENTER
           </Text>
         )}
