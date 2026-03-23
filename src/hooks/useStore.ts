4dc8714548711d3897c669a84c4cd303087bf541
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-23 16:42:41 +0800

    Update useStore.ts

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,23 +1,30 @@
 import { create } from 'zustand';
 
-interface GameState {
+interface StoreState {
   forward: boolean;
   backward: boolean;
   left: boolean;
   right: boolean;
   jump: boolean;
+  // This part just describes the types
+  nearDoor: string | null; 
   setMove: (direction: string, value: boolean) => void;
-  // Add these to your Zustand store interface/state
-nearDoor: null as string | null, // Stores the ID of the house you're near
-setNearDoor: (id: string | null) => set({ nearDoor: id }),
-interact: () => { /* We will handle the logic in the component */ },
+  setNearDoor: (id: string | null) => void;
 }
 
-export const useStore = create<GameState>((set) => ({
+export const useStore = create<StoreState>((set) => ({
+  // 1. Initial Values
   forward: false,
   backward: false,
   left: false,
   right: false,
   jump: false,
-  setMove: (direction, value) => set((state) => ({ ...state, [direction]: value })),
+  nearDoor: null,
+
+  // 2. Actions
+  setMove: (direction, value) => 
+    set((state) => ({ ...state, [direction]: value })),
+
+  setNearDoor: (id) => 
+    set({ nearDoor: id }),
 }));
