dbacd051826682af9a3a1de93cdd8ccf971844bd
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-26 22:08:29 +0800

    Update LightPost.tsx

--- src/app/displayObject/LightPost.tsx
+++ src/app/displayObject/LightPost.tsx
@@ -3,50 +3,29 @@
 import React from "react";
 import { RigidBody } from "@react-three/rapier";
 
-interface LightPostProps {
-  position?: [number, number, number];
-  rotation?: [number, number, number];
-}
-
-export default function LightPost({ 
-  position = [0, 0, 0], 
-  rotation = [0, 0, 0] 
-}: LightPostProps) {
+export default function LightPost({ position = [0, 0, 0], rotation = [0, 0, 0] }) {
   return (
     <group position={position} rotation={rotation}>
       <RigidBody type="fixed" colliders="cuboid">
-        {/* The Pole */}
+        {/* Pole */}
         <mesh castShadow position={[0, 4, 0]}>
-          <cylinderGeometry args={[0.1, 0.15, 8, 8]} />
-          <meshStandardMaterial color="#333333" metalness={0.8} roughness={0.2} />
+          <cylinderGeometry args={[0.1, 0.1, 8, 8]} />
+          <meshStandardMaterial color="#222" />
         </mesh>
 
-        {/* The Arm (Extends 1.5 units) */}
+        {/* Arm - Notice position is relative to the group */}
         <mesh castShadow position={[0.7, 7.8, 0]} rotation={[0, 0, Math.PI / 2]}>
-          <cylinderGeometry args={[0.08, 0.08, 1.5, 8]} />
-          <meshStandardMaterial color="#333333" />
+          <cylinderGeometry args={[0.05, 0.05, 1.5, 8]} />
+          <meshStandardMaterial color="#222" />
         </mesh>
 
-        {/* The Lamp Head */}
-        <mesh position={[1.4, 7.6, 0]}>
-          <boxGeometry args={[0.6, 0.2, 0.4]} />
-          <meshStandardMaterial color="#222222" />
+        {/* Light Head */}
+        <mesh position={[1.4, 7.8, 0]}>
+          <boxGeometry args={[0.5, 0.2, 0.3]} />
+          <meshStandardMaterial color="#111" />
         </mesh>
 
-        {/* Light Source */}
-        <pointLight 
-          position={[1.4, 7.4, 0]} 
-          intensity={25} 
-          distance={30} 
-          color="#fff4d6" 
-          castShadow 
-        />
-        
-        {/* Glowing Bulb */}
-        <mesh position={[1.4, 7.4, 0]}>
-          <sphereGeometry args={[0.15, 16, 16]} />
-          <meshStandardMaterial emissive="#fff4d6" emissiveIntensity={3} color="white" />
-        </mesh>
+        <pointLight position={[1.4, 7.5, 0]} intensity={20} distance={20} color="#ffdfad" castShadow />
       </RigidBody>
     </group>
   );
