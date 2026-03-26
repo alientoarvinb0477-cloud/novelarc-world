61e031c7ac11d5cd30b9cacda6de36f32d1e380a
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-26 14:38:13 +0800

    Update Billboard.tsx

--- src/app/displayObject/Billboard.tsx
+++ src/app/displayObject/Billboard.tsx
@@ -1,75 +1,54 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { Text } from "@react-three/drei";
-import { RigidBody, CuboidCollider } from "@react-three/rapier";
-
-interface BillboardProps {
-  position?: [number, number, number];
-  rotation?: [number, number, number];
-  title?: string;
-  description?: string;
-}
+import { RigidBody } from "@react-three/rapier";
 
 export default function Billboard({ 
-  position = [0, 0, 0], 
-  rotation = [0, 0, 0],
+  position = [5, 0, -10], 
+  rotation = [0, -Math.PI / 4, 0],
   title = "NOVELARC",
-  description = "Digital Architecture Era"
-}: BillboardProps) {
-  
+  description = "Visualizing the Future"
+}: any) {
   return (
     <group position={position} rotation={rotation}>
-      {/* 1. PHYSICAL STRUCTURE (The Frame & Legs) */}
       <RigidBody type="fixed" colliders="cuboid">
-        {/* Main Board Surface */}
-        <mesh castShadow receiveShadow position={[0, 5, 0]}>
+        {/* Main Board */}
+        <mesh castShadow position={[0, 5, 0]}>
           <boxGeometry args={[8, 4, 0.4]} />
-          <meshStandardMaterial color="#1a1a1a" /> {/* Sleek Black Frame */}
+          <meshStandardMaterial color="#1a1a1a" />
         </mesh>
-
-        {/* Left Pole */}
-        <mesh castShadow position={[-3, 1.5, 0]}>
-          <boxGeometry args={[0.3, 3, 0.3]} />
+        {/* Supports */}
+        <mesh castShadow position={[-3, 2.5, 0]}>
+          <boxGeometry args={[0.2, 5, 0.2]} />
           <meshStandardMaterial color="#333" />
         </mesh>
-
-        {/* Right Pole */}
-        <mesh castShadow position={[3, 1.5, 0]}>
-          <boxGeometry args={[0.3, 3, 0.3]} />
+        <mesh castShadow position={[3, 2.5, 0]}>
+          <boxGeometry args={[0.2, 5, 0.2]} />
           <meshStandardMaterial color="#333" />
         </mesh>
       </RigidBody>
 
-      {/* 2. TEXT CONTENT (Using Drei Text) */}
-      <group position={[0, 5, 0.21]}>
-        {/* Main Title Text */}
-        <Text
-          position={[0, 0.6, 0]}
-          fontSize={0.8}
-          color="white"
-          font="/fonts/Montserrat-Bold.ttf" // Optional: path to your font
-          anchorX="center"
+      <group position={[0, 5, 0.22]}>
+        <Text 
+          position={[0, 0.5, 0]} 
+          fontSize={0.7} 
+          color="white" 
+          anchorX="center" 
           anchorY="middle"
         >
           {title}
         </Text>
-
-        {/* Subtitle/URL Text */}
-        <Text
-          position={[0, -0.6, 0]}
-          fontSize={0.3}
-          color="#fb923c" // Novelarc Orange
-          font="/fonts/Montserrat-Regular.ttf"
-          anchorX="center"
+        <Text 
+          position={[0, -0.5, 0]} 
+          fontSize={0.3} 
+          color="#fb923c" 
+          anchorX="center" 
           anchorY="middle"
         >
           {description}
         </Text>
       </group>
-
-      {/* 3. LIGHTING (Subtle glow for the board) */}
-      <pointLight position={[0, 5, 1]} intensity={5} color="#ffffff" distance={10} />
     </group>
   );
 }
