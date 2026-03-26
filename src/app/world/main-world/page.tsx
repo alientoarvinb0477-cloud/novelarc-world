fa5877e15d4dce6c1f42e0151c6e8fe43d9bbbf2
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-26 14:28:37 +0800

    Update page.tsx

--- src/app/world/main-world/page.tsx
+++ src/app/world/main-world/page.tsx
@@ -10,7 +10,6 @@ import LoadingScreen from "../../../components/world/LoadingScreen";
 import MobileControls from "../../../components/world/MobileControls";
 import House from "../../house/House";
 import StartOverlay from "../../../components/world/StartOverlay";
-import Billboard from "../../displayObject/Billboard";
 
 function WorldFloor() {
   const { scene } = useGLTF("/floor.glb");
@@ -92,13 +91,6 @@ export default function MainWorldPage() {
     id="starter-home" 
     position={[15, 0, -15]} 
   />
-
-  <Billboard 
-  position={[10, 0, -5]} 
-  rotation={[0, -Math.PI / 4, 0]} 
-  title="WELCOME TO NOVELARC"
-  description="novelarcDigitalEra.com"
-/>
 
 
             
