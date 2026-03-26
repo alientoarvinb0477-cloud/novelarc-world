4b4cfb876323df212bddfd4046293fdf92e82c55
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-26 15:39:01 +0800

    Update Road.tsx

--- src/app/displayObject/Road.tsx
+++ src/app/displayObject/Road.tsx
@@ -3,40 +3,37 @@
 import React from "react";
 import { RigidBody } from "@react-three/rapier";
 
-interface RoadProps {
-  position?: [number, number, number];
-  rotation?: [number, number, number];
-  length?: number;
-  roadWidth?: number;
-  grassWidth?: number;
-}
-
 export default function Road({ 
-  position = [0, 0, 0], 
-  rotation = [0, 0, 0], 
-  length = 200, 
-  roadWidth = 10,
-  grassWidth = 5
-}: RoadProps) {
+  position = [0, 0.2, 0], // Lifted higher off the floor
+  length = 500, 
+  roadWidth = 12,
+  grassWidth = 8
+}) {
   return (
-    <group position={position} rotation={rotation}>
+    <group position={position}>
       <RigidBody type="fixed" colliders="cuboid">
-        {/* 1. THE GRAY ROAD */}
-        <mesh receiveShadow position={[0, 0.05, 0]}>
-          <boxGeometry args={[roadWidth, 0.1, length]} />
-          <meshStandardMaterial color="#333333" roughness={0.8} />
+        {/* MAIN ROAD - Dark Gray */}
+        <mesh receiveShadow position={[0, 0.1, 0]}>
+          <boxGeometry args={[roadWidth, 0.2, length]} />
+          <meshStandardMaterial color="#222222" roughness={0.9} />
         </mesh>
 
-        {/* 2. LEFT GRASS SIDE */}
-        <mesh receiveShadow position={[-(roadWidth / 2 + grassWidth / 2), 0.04, 0]}>
-          <boxGeometry args={[grassWidth, 0.1, length]} />
-          <meshStandardMaterial color="#2d5a27" roughness={1} />
+        {/* LEFT GRASS - Forest Green */}
+        <mesh receiveShadow position={[-(roadWidth / 2 + grassWidth / 2), 0.05, 0]}>
+          <boxGeometry args={[grassWidth, 0.15, length]} />
+          <meshStandardMaterial color="#1b3d17" />
         </mesh>
 
-        {/* 3. RIGHT GRASS SIDE */}
-        <mesh receiveShadow position={[(roadWidth / 2 + grassWidth / 2), 0.04, 0]}>
-          <boxGeometry args={[grassWidth, 0.1, length]} />
-          <meshStandardMaterial color="#2d5a27" roughness={1} />
+        {/* RIGHT GRASS - Forest Green */}
+        <mesh receiveShadow position={[(roadWidth / 2 + grassWidth / 2), 0.05, 0]}>
+          <boxGeometry args={[grassWidth, 0.15, length]} />
+          <meshStandardMaterial color="#1b3d17" />
+        </mesh>
+
+        {/* OPTIONAL: Center White Line for Visibility */}
+        <mesh position={[0, 0.21, 0]}>
+          <boxGeometry args={[0.2, 0.01, length]} />
+          <meshStandardMaterial color="white" />
         </mesh>
       </RigidBody>
     </group>
