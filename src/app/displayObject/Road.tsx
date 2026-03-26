3a18810bceb43e89e6e4041fd4c411302a26ffbf
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-26 22:27:39 +0800

    Update Road.tsx

--- src/app/displayObject/Road.tsx
+++ src/app/displayObject/Road.tsx
@@ -3,52 +3,30 @@
 import React from "react";
 import { RigidBody } from "@react-three/rapier";
 
-export default function Road({ position = [0, 1.0, 0], length = 3000, roadWidth = 15 }) {
+export default function Road({ position = [0, 1.0, 0], length = 2000, roadWidth = 15 }) {
   return (
     <group position={position as [number, number, number]}>
       <RigidBody type="fixed" colliders="cuboid">
-        {/* ASPHALT - Base Layer */}
+        {/* ASPHALT */}
         <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
           <planeGeometry args={[roadWidth, length]} />
-          <meshStandardMaterial color="#1a1a1a" roughness={0.9} metalness={0.1} />
+          <meshStandardMaterial color="#2a2a2a" roughness={0.8} />
         </mesh>
 
-        {/* YELLOW LINE - Elevated and Offset to prevent "damage" look */}
-        <mesh position={[0, 0.02, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        {/* YELLOW LINE - polygonOffset prevents the "damage" flicker */}
+        <mesh position={[0, 0.01, 0]} rotation={[-Math.PI / 2, 0, 0]}>
           <planeGeometry args={[0.2, length]} />
-          <meshStandardMaterial 
-            color="#ffcc00" 
-            emissive="#ffcc00"
-            emissiveIntensity={0.1}
-            polygonOffset 
-            polygonOffsetFactor={-1} 
-            polygonOffsetUnits={-1} 
-          />
+          <meshStandardMaterial color="#ffcc00" polygonOffset polygonOffsetFactor={-4} />
         </mesh>
 
-        {/* WHITE BOUNDARY LINES - Slightly lower offset than the center line */}
-        <mesh position={[-(roadWidth / 2 - 0.5), 0.015, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        {/* WHITE BOUNDARY LINES */}
+        <mesh position={[-(roadWidth / 2 - 0.5), 0.01, 0]} rotation={[-Math.PI / 2, 0, 0]}>
           <planeGeometry args={[0.2, length]} />
-          <meshStandardMaterial 
-            color="white" 
-            transparent 
-            opacity={0.9} 
-            polygonOffset 
-            polygonOffsetFactor={-1} 
-            polygonOffsetUnits={-1}
-          />
+          <meshStandardMaterial color="white" transparent opacity={0.8} polygonOffset polygonOffsetFactor={-4} />
         </mesh>
-        
-        <mesh position={[(roadWidth / 2 - 0.5), 0.015, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh position={[(roadWidth / 2 - 0.5), 0.01, 0]} rotation={[-Math.PI / 2, 0, 0]}>
           <planeGeometry args={[0.2, length]} />
-          <meshStandardMaterial 
-            color="white" 
-            transparent 
-            opacity={0.9} 
-            polygonOffset 
-            polygonOffsetFactor={-1} 
-            polygonOffsetUnits={-1}
-          />
+          <meshStandardMaterial color="white" transparent opacity={0.8} polygonOffset polygonOffsetFactor={-4} />
         </mesh>
       </RigidBody>
     </group>
