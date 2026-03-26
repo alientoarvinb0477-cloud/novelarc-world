b0c18cbab2419b227c91b2f793e97e3e4286661a
alientoarvinb0477-cloud <aliento.arvin.b.0477@gmail.com>
2026-03-26 22:42:05 +0800

    Update LightPost.tsx

--- src/app/displayObject/LightPost.tsx
+++ src/app/displayObject/LightPost.tsx
@@ -3,29 +3,33 @@
 import React from "react";
 import { RigidBody } from "@react-three/rapier";
 
-export default function LightPost({ position = [0, 0, 0], rotation = [0, 0, 0] }) {
+export default function LightPost({ position = [0, 0, 0] as [number, number, number], rotation = [0, 0, 0] as [number, number, number] }) {
+  // If the position is missing, don't render to prevent crashes
+  if (!position) return null;
+
   return (
     <group position={position} rotation={rotation}>
       <RigidBody type="fixed" colliders="cuboid">
         {/* Pole */}
         <mesh castShadow position={[0, 4, 0]}>
-          <cylinderGeometry args={[0.1, 0.1, 8, 8]} />
+          <boxGeometry args={[0.2, 8, 0.2]} /> {/* Box is more stable than Cylinder for performance */}
           <meshStandardMaterial color="#222" />
         </mesh>
 
-        {/* Arm - Notice position is relative to the group */}
-        <mesh castShadow position={[0.7, 7.8, 0]} rotation={[0, 0, Math.PI / 2]}>
-          <cylinderGeometry args={[0.05, 0.05, 1.5, 8]} />
+        {/* Arm */}
+        <mesh castShadow position={[0.7, 7.8, 0]}>
+          <boxGeometry args={[1.5, 0.1, 0.1]} />
           <meshStandardMaterial color="#222" />
         </mesh>
 
         {/* Light Head */}
         <mesh position={[1.4, 7.8, 0]}>
-          <boxGeometry args={[0.5, 0.2, 0.3]} />
+          <boxGeometry args={[0.6, 0.2, 0.4]} />
           <meshStandardMaterial color="#111" />
         </mesh>
 
-        <pointLight position={[1.4, 7.5, 0]} intensity={20} distance={20} color="#ffdfad" castShadow />
+        {/* Simple Light Source */}
+        <pointLight position={[1.4, 7.4, 0]} intensity={10} distance={15} color="#ffdfad" />
       </RigidBody>
     </group>
   );
